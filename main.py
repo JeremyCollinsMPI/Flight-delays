b50e339889bc0789ab28a139a5976e087268c992
Jeremy Collins <jeremycollins@RobotDatas-MacBook-Pro-2.local>
2019-09-17 12:23:44 +0800

    added epochs

--- main.py
+++ main.py
@@ -150,6 +150,7 @@ loss_function = 'mean_squared_error'
 dataframe = create_dataframe(file)
 feature_layer = create_feature_layer(dataframe)
 train_ds, validation_ds, test_ds = create_datasets(dataframe)
+epochs = 60
 if args.mode == 'train':
   train(train_ds, validation_ds, test_ds, feature_layer, loss_function, epochs)
 if args.mode == 'evaluate':
